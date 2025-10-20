01f340cff72f4b327926c449c3146f91db7e5526
sudarshan233 <nsridharsudarshan@gmail.com>
2025-10-20 16:01:57 +0530

    Progress: 1. Established the route for POST API or Create User option
    Next Step:
    1. Established the route for PATCH API or Edit User option
    2. Established the route for GET API or View User option
    3. Established the route for DELETE API or Delete User option
    4. Established the route for GET API or Viewing all users option

---
+++ main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+type User struct {
+	ID                int    `json:"id"`
+	UserType          string `json:"userType"`
+	UserRole          string `json:"userRole"`
+	FirstName         string `json:"firstName"`
+	LastName          string `json:"lastName"`
+	PhoneNumber       int64  `json:"phoneNumber"`
+	EmailID           string `json:"email"`
+	Password          string `json:"password"`
+	ConfirmPassword   string `json:"confirmPassword"`
+	UserCurrency      string `json:"userCurrency"`
+	NumberFormat      string `json:"numberFormat"`
+	MeasurementSystem string `json:"measurementSystem"`
+	DecimalPlaces     int    `json:"decimalPlaces"`
+	UserStatus        bool   `json:"userStatus"`
+	UserTeam          string `json:"userTeam"`
+}
+
+func main() {
+	fmt.Println("Hello World")
+
+	app := fiber.New()
+	users := []User{}
+
+	app.Post("/api/create", func(c *fiber.Ctx) error {
+		user := &User{}
+
+		if err := c.BodyParser(user); err != nil {
+			return err
+		}
+
+		user.ID = len(users) + 1
+		users = append(users, *user)
+
+		return c.Status(201).JSON(fiber.Map{
+			"msg":  "User created successfully!!",
+			"user": user,
+		})
+
+	})
+
+	log.Fatal(app.Listen(":5000"))
+}
