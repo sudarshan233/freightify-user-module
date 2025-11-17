75cd0f33e70f7c935078517d85e188ad6444f767
Sudarshan NS <sudarshanns@SudarshanNS.local>
2025-11-17 17:55:38 +0530

    Progress: 1. The toast message after creation and editing of user profile has been executed successfully. 2. The confirmaltion dialog before deleting the user profile has been executed successfully.

--- frontend/src/app/components/toast-basic-demo/toast-basic-demo.ts
+++ frontend/src/app/components/toast-basic-demo/toast-basic-demo.ts
@@ -2,13 +2,11 @@ import { Component } from '@angular/core';
 import { MessageService } from 'primeng/api';
 import { Toast } from 'primeng/toast';
 import { ButtonModule } from 'primeng/button';
-import { Ripple } from 'primeng/ripple';
-
 @Component({
     selector: 'toast-basic-demo',
     templateUrl: './toast-basic-demo.html',
     standalone: true,
-    imports: [Toast, Ripple, ButtonModule],
+    imports: [Toast, ButtonModule],
     providers: [MessageService]
 })
 export class ToastBasicDemo {}
